a62b144609e964b82482bb1d573f447285474ef7
Russel McMillan <rfmcmillan@gmail.com>
2022-02-18 09:48:30 -0500

    added config files

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const PORT = process.env.PORT || 8080;
+const app = require("./app");
+
+const init = async () => {
+  try {
+    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  } catch (ex) {
+    console.log(ex);
+  }
+};
+
+init();
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  entry: ["./client/index.js"],
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  performance: {
+    maxAssetSize: 300000,
+    maxEntrypointSize: 300000,
+  },
+};
