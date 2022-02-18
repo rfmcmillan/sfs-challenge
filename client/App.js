ed5a38e3fe6d31974e7d7240ee79b9ef8c2fc324
Russel McMillan <rfmcmillan@gmail.com>
2022-02-18 13:24:10 -0500

    now fetching data from provided url

---
+++ client/App.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import DebtTable from "./components/DebtTable";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 330,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#1E9AAA",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#06d6a0",
+      contrastText: "#4e4e4e",
+    },
+    text: {
+      primary: "#4e4e4e",
+    },
+    info: {
+      main: "#fdb000",
+      contrastText: "#F8F8F8",
+    },
+    success: {
+      main: "#06d6a0",
+      contrastText: "#4e4e4e",
+    },
+  },
+});
+
+const App = () => {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const response = axios
+      .get(
+        "https://raw.githubusercontent.com/StrategicFS/Recruitment/master/data.json"
+      )
+      .then((response) => {
+        const { data } = response;
+        console.log("🚀 ~ file: App.js ~ line 48 ~ .then ~ data", data);
+        setData(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <DebtTable data={data} />{" "}
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ client/components/DebtTable.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DebtTable = (props) => {
+  const { data } = props;
+  console.log("🚀 ~ file: DebtTable.js ~ line 5 ~ DebtTable ~ data", data);
+
+  return <div></div>;
+};
+
+export default DebtTable;
