73605f14ea80f0c443abe531b8c6a06e021b38ae
Russel McMillan <rfmcmillan@gmail.com>
2022-02-22 13:34:28 -0500

    removed theme

--- client/App.js
+++ client/App.js
@@ -1,46 +1,9 @@
 import React from "react";
-import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+
 import Home from "./components/Home";
 
-const theme = createTheme({
-  breakpoints: {
-    values: {
-      xs: 330,
-      sm: 600,
-      md: 900,
-      lg: 1200,
-      xl: 1536,
-    },
-  },
-  palette: {
-    primary: {
-      main: "#1E9AAA",
-      contrastText: "#ffffff",
-    },
-    secondary: {
-      main: "#06d6a0",
-      contrastText: "#4e4e4e",
-    },
-    text: {
-      primary: "#4e4e4e",
-    },
-    info: {
-      main: "#fdb000",
-      contrastText: "#F8F8F8",
-    },
-    success: {
-      main: "#06d6a0",
-      contrastText: "#4e4e4e",
-    },
-  },
-});
-
 const App = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
-  );
+  return <Home />;
 };
 
 export default App;
